7cce8dfbe4f0c0cf053e8c71896428e1a4f348b7
Constantine-PC\Constantine <twisted_terr0ri@hotmail.com>
2021-07-29 00:39:49 +0300

    dnd almost working

--- src/components/ChampSlot.js
+++ src/components/ChampSlot.js
@@ -1,20 +1,34 @@
 import React from "react";
+import { connect } from "react-redux";
+import {getSelection, getSelectionIndex} from "../reducers/reducer"
 import { Droppable } from "react-beautiful-dnd";
+import Card from "./Card";
 
-export default function ChampSlot({playerId}) {
+ function ChampSlot({playerId, selection,selectionIndex}) {
   return (
     <div className="champDiv">
       <Droppable droppableId={playerId} key={playerId}>
         {(provided) => (
           <div
-            className="champSpot"
+            className={"champSpot"}
             ref={provided.innerRef}
             {...provided.droppableProps}
           >
-            {provided.placeholder}
+            {selection != null && <Card champ={selection} index={selectionIndex}/> }
           </div>
         )}
       </Droppable>
     </div>
   );
 }
+
+const mapStateToProps = (state,ownProps) => ({
+   selection: getSelection(state,ownProps.playerId),
+   selectionIndex: getSelectionIndex(state, ownProps.playerId)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+});
+
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChampSlot);
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,7 +1,6 @@
 import { champions } from "../app/heroes";
 import { SEARCH_CHANGED, DRAG_END } from "../actions/actions";
 
-
 export const getFilteredChampions = (state) => {
   return state.filteredChampions;
 };
@@ -14,7 +13,7 @@ const filteredChampions = (state = champions, action) => {
       });
     case DRAG_END:
       return state.filter((champ) => {
-        return champ !== action.drag
+        return champ !== action.drag;
       });
     default:
       return state;
@@ -25,9 +24,27 @@ export const getChampions = (state) => {
   return state.champions;
 };
 
+export const getSelection = (state,playerId) => state.selections[playerId]
+export const getSelectionIndex = (state,playerId) => state.filteredChampions.indexOf(state.selections[playerId])
+
+const selections = (state = {}, action) => {
+  switch (action.type) {
+    case DRAG_END:
+      // if (action.drop.startsWith("teamplayer")) return [...state, action.drag];
+      // else return state;
+      return {
+        ...state,
+        [action.drop]: action.drag
+      }
+    default:
+      return state;
+  }
+};
+
 const reducer = (state = {}, action) => ({
   champions: champions,
   filteredChampions: filteredChampions(state.filteredChampions, action),
+  selections: selections(state.selections, action),
 });
 
 export default reducer;
