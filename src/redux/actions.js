c305d74336921020784f926a389549a651bb17c4
Constantine <kostaskaldis@gmail.com>
2021-08-19 18:06:21 +0300

    actions file formating

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,14 +1,13 @@
 export const SEARCH_CHANGED = "SEARCH_CHANGED";
 export const DRAG_END = "DRAG_END";
-export const CHANGE_PREVIEW = "CHANGE_PREVIEW"
-
+export const CHANGE_PREVIEW = "CHANGE_PREVIEW";
 
 export const searchChanged = (lookup) => ({
   type: SEARCH_CHANGED,
   lookup, // lookup: lookup / eqivalent code
 });
 
-export const previewStyle = (viewSelection) =>({
+export const previewStyle = (viewSelection) => ({
   type: CHANGE_PREVIEW,
   viewSelection,
 });
