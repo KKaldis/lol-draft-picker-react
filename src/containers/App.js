d3c5877140869f7ae518cfba2382e4ac377187b5
Constantine-PC\Constantine <twisted_terr0ri@hotmail.com>
2021-08-23 15:36:32 +0300

    find counters

--- src/containers/App.js
+++ src/containers/App.js
@@ -11,30 +11,31 @@ import data from "../app/data.json";
 
 const App = ({ onDragEnd, sorting, tier, selections}) => {
 
-  var oldRating = {};
-  Object.entries(selections).forEach(([keyChampDiv, valChamp]) => {
-    if (keyChampDiv.startsWith("enemy")) {
-      // console.log(valChamp);
-      Object.entries(data).forEach(([keyChamp, valLane]) => {
-        if (keyChamp.toUpperCase() === valChamp.toUpperCase()) {
-          const laneKeyObj = valLane;
-          Object.entries(laneKeyObj).forEach(([keyLane, valTier]) => {
-            var newRating = valTier[tier][sorting];
-            // iterate over map2 entries with acc set to map1 at start
-            oldRating = Object.entries(newRating).reduce(
-              (acc, [key, value]) =>
-                // if key is already in map1, add the values, otherwise, create new pair
-                ({ ...acc, [key]: (acc[key] || 0) + value }),
-              { ...oldRating }
-            );
-            // console.log(valChamp, " : " ,keyLane, " : ", newRating);
-          });
-        }
-      });
-    }
-  });
-  console.log(" Champ Scores ", oldRating);
-
+  if (sorting === "Rating" || sorting === "Popular") {
+    var oldRating = {};
+    Object.entries(selections).forEach(([keyChampDiv, valChamp]) => {
+      if (keyChampDiv.startsWith("enemy")) {
+        // console.log(valChamp);
+        Object.entries(data).forEach(([keyChamp, valLane]) => {
+          if (keyChamp.toUpperCase() === valChamp.toUpperCase()) {
+            const laneKeyObj = valLane;
+            Object.entries(laneKeyObj).forEach(([keyLane, valTier]) => {
+              var newRating = valTier[tier][sorting];
+              // iterate over map2 entries with acc set to map1 at start
+              oldRating = Object.entries(newRating).reduce(
+                (acc, [key, value]) =>
+                  // if key is already in map1, add the values, otherwise, create new pair
+                  ({ ...acc, [key]: (acc[key] || 0) + value }),
+                { ...oldRating }
+              );
+              // console.log(valChamp, " : " ,keyLane, " : ", newRating);
+            });
+          }
+        });
+      }
+    });
+    console.log(" Champ Scores ", oldRating);
+  }
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
