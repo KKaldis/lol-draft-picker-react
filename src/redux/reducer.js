7ed4b6fdbe50f7092124dcb7e02571c9debc301f
Constantine-PC\Constantine <twisted_terr0ri@hotmail.com>
2021-08-23 04:32:23 +0300

    reducer corection

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -154,31 +154,24 @@ export const getTier = (state) => {
 export const teamSelections = (state = [], action, teamString) => {
   switch (action.type) {
     case DRAG_END:
-      // const sel = [];
-      // Object.entries(selections).forEach(([key, value]) => {
-      //   if (key.startsWith(teamString)) {
-      //     sel.push(value);
-      //   }
-      // });
-      // return [...state], sel;
-      if (
-        action.sourceDroppable.startsWith(teamString) &&
-        action.destinationDroppable.startsWith(teamString)
-      ) {
+      if (action.sourceDroppable.startsWith(teamString) &&
+        action.destinationDroppable.startsWith(teamString)){
+        return [...state]
       } else if (action.destinationDroppable.startsWith(teamString)) {
         return [...state, action.sourceDraggable];
-      } else if (
-        !action.destinationDroppable.startsWith(teamString) &&
-        action.sourceDroppable.startsWith(teamString)
-      ) {
+      } else if (!action.destinationDroppable.startsWith(teamString) &&
+        action.sourceDroppable.startsWith(teamString)) {
         var arrRemove = state;
         var arrIndex = arrRemove.indexOf(action.sourceDraggable);
         arrRemove.splice(arrIndex, 1);
-        return state, arrRemove;
+        return arrRemove
       }
-
+      else {
+        return [...state]
+      }
+      
     case CHANGE_PREVIEW:
-      if (action.viewSelection === "Reset") return {};
+      if (action.viewSelection === "Reset") return [];
       else {
         return [...state];
       }
