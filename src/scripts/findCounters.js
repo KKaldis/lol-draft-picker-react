bbdfe85c37601b07947254d591e8a2e4722d1ccd
Constantine <kostaskaldis@gmail.com>
2021-08-25 02:45:31 +0300

    find counters code cleanup

--- src/scripts/findCounters.js
+++ src/scripts/findCounters.js
@@ -5,25 +5,18 @@ export const countAllChamps = (type, tier, sorting, selections) => {
     var oldRating = {};
     Object.entries(selections).forEach(([keyChampDiv, valChamp]) => {
       if (keyChampDiv.startsWith(type)) {
-        // console.log(valChamp);
-        Object.entries(data).forEach(([keyChamp, valLane]) => {
-          if (
-            keyChamp.toUpperCase().replace(/([^\w]+|\s+)/g, "") ===
-            valChamp.toUpperCase().replace(/([^\w]+|\s+)/g, "")
-          ) {
-            const laneKeyObj = valLane;
-            Object.entries(laneKeyObj).forEach(([keyLane, valTier]) => {
-              var newRating = valTier[tier][sorting];
-              oldRating = Object.entries(newRating).reduce(
-                (acc, [key, value]) => ({
-                  ...acc,
-                  [key]: (acc[key] || 0) + value,
-                }),
-                { ...oldRating }
-              );
-              // console.log(valChamp, " : " ,keyLane, " : ", newRating);
-            });
-          }
+        const laneKeyObj = data[valChamp];
+
+        Object.entries(laneKeyObj).forEach(([keyLane, valTier]) => {
+          var newRating = valTier[tier][sorting];
+          oldRating = Object.entries(newRating).reduce(
+            (acc, [key, value]) => ({
+              ...acc,
+              [key]: (acc[key] || 0) + value,
+            }),
+            { ...oldRating }
+          );
+          // console.log(valChamp, " : " ,keyLane, " : ", newRating);
         });
       }
     });
@@ -32,13 +25,6 @@ export const countAllChamps = (type, tier, sorting, selections) => {
       Object.entries(oldRating).sort(([, a], [, b]) => b - a)
     );
 
-    // console.log(type, "counter results : ", highestSort);
-    // print json with object order
-    // console.log(
-    //   type,
-    //   "counter results : ",
-    //   JSON.stringify(highestSort, null, 4)
-    // );
     return highestSort;
   }
 };
