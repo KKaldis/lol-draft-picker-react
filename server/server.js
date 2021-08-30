45cfb33197bfced0cfb75a3574f265bdbd3a490c
Constantine-PC\Constantine <twisted_terr0ri@hotmail.com>
2021-08-30 21:19:01 +0300

    sql attempt

--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 const express = require("express");
 const app = express();
+const mysql = require("mysql");
+
+const db = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  port: "3306",
+  password: "segamao247",
+  database: "counter_data",
+});
 
 app.get("/", (req, res) => {
-  res.send("test response");
+  const sqlInsert =
+    "INSERT INTO tierlist (tier) VALUES ('Master');";
+  db.query(sqlInsert, (err, result) => {
+    console.log(err)
+    console.log(result)
+    res.send("test response");
+  });
 });
 
 app.listen(3001, () => {
